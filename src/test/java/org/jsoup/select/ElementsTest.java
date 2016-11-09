db6e81bf4c9223125c7089a593f71504cb302230
Ilya Idamkin <ilya.idamkin@duallab.com>
2016-11-09 14:43:32 +0300

    Change jsoup tests related to resolving of URI.
    In C# symbol "/" is added if absent to the URI's path, while in Java it stays the same as in the input string. Though both variants are correct the changes were made so that tests work in the same way for C# and Java. Also there are some differences in resolving file URIs.
    
    DEVSIX-902

--- src/test/java/org/jsoup/select/ElementsTest.java
+++ src/test/java/org/jsoup/select/ElementsTest.java
@@ -72,14 +72,14 @@ public class ElementsTest {
     }
 
     @Test public void absAttr() {
-        Document doc = Jsoup.parse("<a id=1 href='/foo'>One</a> <a id=2 href='http://jsoup.org'>Two</a>");
+        Document doc = Jsoup.parse("<a id=1 href='/foo'>One</a> <a id=2 href='http://jsoup.org/'>Two</a>");
         Elements one = doc.select("#1");
         Elements two = doc.select("#2");
         Elements both = doc.select("a");
 
         assertEquals("", one.attr("abs:href"));
-        assertEquals("http://jsoup.org", two.attr("abs:href"));
-        assertEquals("http://jsoup.org", both.attr("abs:href"));
+        assertEquals("http://jsoup.org/", two.attr("abs:href"));
+        assertEquals("http://jsoup.org/", both.attr("abs:href"));
     }
 
     @Test public void classes() {
