45eb536d5891fd7f956a9760b4c14627beb6729c
Yulian Gaponenko <yulian.gaponenko@duallab.com>
2017-01-20 14:48:19 +0300

    Support page at-rule properties
    DEVSIX-1020

--- src/main/java/com/itextpdf/html2pdf/LogMessageConstant.java
+++ src/main/java/com/itextpdf/html2pdf/LogMessageConstant.java
@@ -52,23 +52,24 @@ public final class LogMessageConstant {
     public static final String INPUT_TYPE_IS_NOT_SUPPORTED = "Input type {0} is not supported";
     public static final String INVALID_CSS_PROPERTY_DECLARATION = "Invalid css property declaration: {0}";
     public static final String MARGIN_VALUE_IN_PERCENT_NOT_SUPPORTED = "Margin value in percents not supported";
+    public static final String NOT_SUPPORTED_LIST_STYLE_TYPE = "Not supported list style type: {0}";
     public static final String NO_CONSUMER_FOUND_FOR_CONTENT = "No consumer found for content";
     public static final String NO_CSS_APPLIER_FOUND_FOR_TAG = "No css applier found for tag {0}";
     public static final String NO_WORKER_FOUND_FOR_TAG = "No worker found for tag {0}";
-    public static final String NOT_SUPPORTED_LIST_STYLE_TYPE = "Not supported list style type: {0}";
     public static final String PADDING_VALUE_IN_PERCENT_NOT_SUPPORTED = "Padding value in percents not supported";
+    public static final String PAGE_SIZE_VALUE_IS_INVALID = "Page size value {0} is invalid.";
     public static final String QUOTE_IS_NOT_CLOSED_IN_CSS_EXPRESSION = "The quote is not closed in css expression: {0}";
     public static final String RULE_IS_NOT_SUPPORTED = "The rule @{0} is unsupported. All selectors in this rule will be ignored.";
-    public static final String TEXT_WAS_NOT_PROCESSED = "Text was not processed: {0}";
     public static final String TEXT_DECORATION_BLINK_NOT_SUPPORTED = "text-decoration: blink not supported";
+    public static final String TEXT_WAS_NOT_PROCESSED = "Text was not processed: {0}";
     public static final String UNABLE_TO_PROCESS_EXTERNAL_CSS_FILE = "Unable to process external css file";
-    public static final String UNABLE_TO_RETRIEVE_IMAGE_WITH_GIVEN_BASE_URI = "Unable to retrieve image with given base URI ({0}) and image source path ({1})";
-    public static final String UNABLE_TO_RETRIEVE_IMAGE_FROM_BASE64_SOURCE = "Unable to retrieve image from given base64 source string";
     public static final String UNABLE_TO_RESOLVE_FONT = "Unable to resolve font: {0}. The default one will be used instead";
+    public static final String UNABLE_TO_RETRIEVE_IMAGE_FROM_BASE64_SOURCE = "Unable to retrieve image from given base64 source string";
+    public static final String UNABLE_TO_RETRIEVE_IMAGE_WITH_GIVEN_BASE_URI = "Unable to retrieve image with given base URI ({0}) and image source path ({1})";
     public static final String UNKNOWN_ABSOLUTE_METRIC_LENGTH_PARSED = "Unknown absolute metric length parsed \"{0}\".";
     public static final String WAS_NOT_ABLE_TO_DEFINE_BACKGROUND_CSS_SHORTHAND_PROPERTIES = "Was not able to define one of the background CSS shorthand properties: {0}";
-    public static final String WORKER_UNABLE_TO_PROCESS_OTHER_WORKER = "Worker of type {0} unable to process {1}";
     public static final String WORKER_UNABLE_TO_PROCESS_IT_S_TEXT_CONTENT = "Worker of type {0} unable to process it's text content";
+    public static final String WORKER_UNABLE_TO_PROCESS_OTHER_WORKER = "Worker of type {0} unable to process {1}";
 
     private LogMessageConstant() {
     }
---
+++ src/main/java/com/itextpdf/html2pdf/attach/impl/layout/PageSizeParser.java
@@ -0,0 +1,157 @@
+/*
+    This file is part of the iText (R) project.
+    Copyright (c) 1998-2017 iText Group NV
+    Authors: iText Software.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License version 3
+    as published by the Free Software Foundation with the addition of the
+    following permission added to Section 15 as permitted in Section 7(a):
+    FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY
+    ITEXT GROUP. ITEXT GROUP DISCLAIMS THE WARRANTY OF NON INFRINGEMENT
+    OF THIRD PARTY RIGHTS
+
+    This program is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU Affero General Public License for more details.
+    You should have received a copy of the GNU Affero General Public License
+    along with this program; if not, see http://www.gnu.org/licenses or write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA, 02110-1301 USA, or download the license from the following URL:
+    http://itextpdf.com/terms-of-use/
+
+    The interactive user interfaces in modified source and object code versions
+    of this program must display Appropriate Legal Notices, as required under
+    Section 5 of the GNU Affero General Public License.
+
+    In accordance with Section 7(b) of the GNU Affero General Public License,
+    a covered work must retain the producer line in every PDF that is created
+    or manipulated using iText.
+
+    You can be released from the requirements of the license by purchasing
+    a commercial license. Buying such a license is mandatory as soon as you
+    develop commercial activities involving the iText software without
+    disclosing the source code of your own applications.
+    These activities include: offering paid services to customers as an ASP,
+    serving PDFs on the fly in a web application, shipping iText with a closed
+    source product.
+
+    For more information, please contact iText Software Corp. at this
+    address: sales@itextpdf.com
+ */
+package com.itextpdf.html2pdf.attach.impl.layout;
+
+import com.itextpdf.html2pdf.LogMessageConstant;
+import com.itextpdf.html2pdf.css.CssConstants;
+import com.itextpdf.html2pdf.css.util.CssUtils;
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.layout.property.UnitValue;
+import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class PageSizeParser {
+    private static Map<String, PageSize> pageSizeConstants = new HashMap<>();
+    static {
+        pageSizeConstants.put("a5", PageSize.A5);
+        pageSizeConstants.put("a4", PageSize.A4);
+        pageSizeConstants.put("a3", PageSize.A3);
+        pageSizeConstants.put("b5", PageSize.B5);
+        pageSizeConstants.put("b4", PageSize.B4);
+        pageSizeConstants.put("jis-b5", new PageSize(516, 729));
+        pageSizeConstants.put("jis-b4", new PageSize(729, 1032));
+        pageSizeConstants.put("letter", PageSize.LETTER);
+        pageSizeConstants.put("legal", PageSize.LEGAL);
+        pageSizeConstants.put("ledger", PageSize.LEDGER); // TODO may be use here TABLOID? based on w3c tests, ledger in html is interpreted as portrait-oriented page
+    }
+
+    static PageSize fetchPageSize(String pageSizeStr, float em, float rem, PageSize defaultPageSize) {
+        PageSize pageSize = defaultPageSize.clone();
+        if (pageSizeStr == null || CssConstants.AUTO.equals(pageSizeStr)) {
+            return pageSize;
+        }
+        
+        String[] pageSizeChunks = pageSizeStr.split(" ");
+        String firstChunk = pageSizeChunks[0];
+
+        if (isLengthValue(firstChunk)) {
+            PageSize pageSizeBasedOnLength = parsePageLengthValue(pageSizeChunks, em, rem);
+            if (pageSizeBasedOnLength != null) {
+                pageSize = pageSizeBasedOnLength;
+            } else {
+                Logger logger = LoggerFactory.getLogger(PageSizeParser.class);
+                logger.error(MessageFormat.format(LogMessageConstant.PAGE_SIZE_VALUE_IS_INVALID, pageSizeStr));
+            }
+        } else {
+            Boolean landscape = null;
+            PageSize namedPageSize = null;
+
+            if (isLandscapePortraitValue(firstChunk)) {
+                landscape = CssConstants.LANDSCAPE.equals(firstChunk);
+            } else {
+                namedPageSize = pageSizeConstants.get(firstChunk);
+            }
+
+            if (pageSizeChunks.length > 1) {
+                String secondChunk = pageSizeChunks[1];
+                if (isLandscapePortraitValue(secondChunk)) {
+                    landscape = CssConstants.LANDSCAPE.equals(secondChunk);
+                } else {
+                    namedPageSize = pageSizeConstants.get(secondChunk);
+                }
+            }
+
+            boolean b1 = pageSizeChunks.length == 1 && (namedPageSize != null || landscape != null); // TODO rename propertly?
+            boolean b2 = namedPageSize != null && landscape != null;
+            if (b1 || b2) {
+                if (namedPageSize != null) {
+                    pageSize = namedPageSize;
+                }
+                if (Boolean.TRUE.equals(landscape)) {
+                    pageSize = pageSize.rotate();
+                }
+            } else {
+                Logger logger = LoggerFactory.getLogger(PageSizeParser.class);
+                logger.error(MessageFormat.format(LogMessageConstant.PAGE_SIZE_VALUE_IS_INVALID, pageSizeStr));
+            }
+        }
+
+        return pageSize;
+    }
+
+    private static PageSize parsePageLengthValue(String[] pageSizeChunks, float em, float rem) {
+        Float width, height;
+        width = tryParsePageLengthValue(pageSizeChunks[0], em, rem);
+        if (width == null) {
+            return null;
+        }
+        if (pageSizeChunks.length > 1) {
+            height = tryParsePageLengthValue(pageSizeChunks[1], em, rem);
+            if (height == null) {
+                return null;
+            }
+        } else {
+            height = width;
+        }
+        return new PageSize((float) width, (float) height);
+    }
+
+    private static Float tryParsePageLengthValue(String valueChunk, float em, float rem) {
+        UnitValue unitValue = CssUtils.parseLengthValueToPt(valueChunk, em, rem);
+        if (unitValue == null || unitValue.isPercentValue()) {
+            return null;
+        }
+        return unitValue.getValue();
+    }
+
+    private static boolean isLengthValue(String pageSizeChunk) {
+        return CssUtils.isMetricValue(pageSizeChunk) || CssUtils.isRelativeValue(pageSizeChunk);
+    }
+
+    private static boolean isLandscapePortraitValue(String pageSizeChunk) {
+        return CssConstants.LANDSCAPE.equals(pageSizeChunk) || CssConstants.PORTRAIT.equals(pageSizeChunk);
+    }
+}
