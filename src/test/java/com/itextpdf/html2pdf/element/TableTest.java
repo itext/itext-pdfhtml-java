9976e23c6e3a07f7ddba2610ebde0605c473999a
Yulian Gaponenko <yulian.gaponenko@duallab.com>
2017-06-27 16:23:58 +0300

    Apply box-sizing property on html2pdf level; add tests for box-sizing proprety
    DEVSIX-1174

--- src/test/java/com/itextpdf/html2pdf/element/TableTest.java
+++ src/test/java/com/itextpdf/html2pdf/element/TableTest.java
@@ -198,6 +198,16 @@ public class TableTest extends ExtendedITextTest {
     }
 
     @Test
+    public void helloTableAuto16DocumentTest() throws IOException, InterruptedException {
+        runTest("hello_table_auto16");
+    }
+
+    @Test
+    public void helloTableAuto17DocumentTest() throws IOException, InterruptedException {
+        runTest("hello_table_auto17");
+    }
+
+    @Test
     public void helloTableHeaderFooterDocumentTest() throws IOException, InterruptedException {
         runTest("hello_table_header_footer");
     }
@@ -311,7 +321,7 @@ public class TableTest extends ExtendedITextTest {
     }
 
     @Test
-    @Ignore("DEVSIX-994, DEVSIX-1174")
+    @Ignore("DEVSIX-994")
     public void tableCollapseColCellBoxSizingWidthDifference() throws IOException, InterruptedException {
         runTest("table_collapse_col_cell_box_sizing_width_difference");
     }
