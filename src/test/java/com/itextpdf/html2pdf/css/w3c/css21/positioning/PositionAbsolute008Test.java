960e86e92af4388b67ae337d232388b2e3790cbf
Pavel Morozov <pavel.morozov@duallab.com>
2017-06-30 14:37:26 +0300

    Implement float elements splitting
    DEVSIX-1267

--- src/test/java/com/itextpdf/html2pdf/css/w3c/css21/positioning/PositionAbsolute008Test.java
+++ src/test/java/com/itextpdf/html2pdf/css/w3c/css21/positioning/PositionAbsolute008Test.java
@@ -44,6 +44,7 @@ package com.itextpdf.html2pdf.css.w3c.css21.positioning;
 
 import com.itextpdf.html2pdf.css.w3c.W3CCssTest;
 
+// TODO DEVSIX-1267
 public class PositionAbsolute008Test extends W3CCssTest {
     @Override
     protected String getHtmlFileName() {
