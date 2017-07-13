94ea4e435c0deb22b1fccc5c62fb779d1e7a68c5
joris.schellekens <joris.schellekens@itextpdf.com>
2017-07-13 11:41:41 +0200

    change BR TagWorker to use correct font
    add test to check new functionality
    
    resolves DEVSIX-1430

--- src/test/java/com/itextpdf/html2pdf/css/BrTagTest.java
+++ src/test/java/com/itextpdf/html2pdf/css/BrTagTest.java
@@ -1,8 +1,8 @@
 /*
     This file is part of the iText (R) project.
     Copyright (c) 1998-2017 iText Group NV
-    Authors: Bruno Lowagie, Paulo Soares, et al.
-    
+    Authors: iText Software.
+
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License version 3
     as published by the Free Software Foundation with the addition of the
@@ -10,7 +10,7 @@
     FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY
     ITEXT GROUP. ITEXT GROUP DISCLAIMS THE WARRANTY OF NON INFRINGEMENT
     OF THIRD PARTY RIGHTS
-    
+
     This program is distributed in the hope that it will be useful, but
     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
     or FITNESS FOR A PARTICULAR PURPOSE.
@@ -20,15 +20,15 @@
     the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
     Boston, MA, 02110-1301 USA, or download the license from the following URL:
     http://itextpdf.com/terms-of-use/
-    
+
     The interactive user interfaces in modified source and object code versions
     of this program must display Appropriate Legal Notices, as required under
     Section 5 of the GNU Affero General Public License.
-    
+
     In accordance with Section 7(b) of the GNU Affero General Public License,
     a covered work must retain the producer line in every PDF that is created
     or manipulated using iText.
-    
+
     You can be released from the requirements of the license by purchasing
     a commercial license. Buying such a license is mandatory as soon as you
     develop commercial activities involving the iText software without
@@ -36,68 +36,66 @@
     These activities include: offering paid services to customers as an ASP,
     serving PDFs on the fly in a web application, shipping iText with a closed
     source product.
-    
+
     For more information, please contact iText Software Corp. at this
     address: sales@itextpdf.com
  */
-package com.itextpdf.html2pdf.attach.impl.tags;
+package com.itextpdf.html2pdf.css;
 
-import com.itextpdf.html2pdf.attach.ITagWorker;
-import com.itextpdf.html2pdf.attach.ProcessorContext;
-import com.itextpdf.html2pdf.css.CssConstants;
-import com.itextpdf.html2pdf.css.util.CssUtils;
-import com.itextpdf.html2pdf.html.AttributeConstants;
-import com.itextpdf.html2pdf.html.node.IElementNode;
-import com.itextpdf.layout.IPropertyContainer;
-import com.itextpdf.layout.element.Text;
+import com.itextpdf.html2pdf.HtmlConverter;
+import com.itextpdf.layout.ElementPropertyContainer;
+import com.itextpdf.layout.element.IElement;
+import com.itextpdf.test.annotations.type.IntegrationTest;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * TagWorker class for the {@code br} element.
+ * This test handles the case where a br tag causes the pdf to no longer be pdf/A compliant
+ * The underlying problem turns out to be that the inserted Text IElement has no font, and uses the default (Helvetica) font.
+ * The font does not get embedded, and as such, it breaks the compliancy.
  */
-public class BrTagWorker implements ITagWorker {
+@Category(IntegrationTest.class)
+public class BrTagTest {
 
-    /** A new line Text element. */
-    private Text newLine = new Text("\n");
-
-    /**
-     * Creates a new {@link BrTagWorker} instance.
-     *
-     * @param element the element
-     * @param context the context
-     */
-    public BrTagWorker(IElementNode element, ProcessorContext context) {
-        String fontFamily = element.getStyles().get(CssConstants.FONT_FAMILY);
-        newLine.setFont(fontFamily);
+    @BeforeClass
+    public static void beforeClass() {
     }
 
-    /* (non-Javadoc)
-     * @see com.itextpdf.html2pdf.attach.ITagWorker#processEnd(com.itextpdf.html2pdf.html.node.IElementNode, com.itextpdf.html2pdf.attach.ProcessorContext)
-     */
-    @Override
-    public void processEnd(IElementNode element, ProcessorContext context) {
-    }
+    @Test
+    public void brTagTest() throws IOException, InterruptedException {
+        String input = "<html>\n" +
+                "<head><title>Test</title></head>\n" +
+                "<body style=\"font-family: FreeSans;\">\n" +
+                "<h1>Test</h1>\n" +
+                "<br />\n" +
+                "<p>Hello World</p>\n" +
+                "</body>\n" +
+                "</html>";
 
-    /* (non-Javadoc)
-     * @see com.itextpdf.html2pdf.attach.ITagWorker#processContent(java.lang.String, com.itextpdf.html2pdf.attach.ProcessorContext)
-     */
-    @Override
-    public boolean processContent(String content, ProcessorContext context) {
-        return false;
-    }
+        Map<String, Integer> fontFrequency = new HashMap<>();
+        for (IElement e : HtmlConverter.convertToElements(input)) {
+            if (e instanceof ElementPropertyContainer) {
+                String fontName = ((ElementPropertyContainer) e).getProperty(20).toString();
+                if (!fontFrequency.containsKey(fontName))
+                    fontFrequency.put(fontName, 0);
+                fontFrequency.put(fontName, fontFrequency.get(fontName) + 1);
+            }
+        }
 
-    /* (non-Javadoc)
-     * @see com.itextpdf.html2pdf.attach.ITagWorker#processTagChild(com.itextpdf.html2pdf.attach.ITagWorker, com.itextpdf.html2pdf.attach.ProcessorContext)
-     */
-    @Override
-    public boolean processTagChild(ITagWorker childTagWorker, ProcessorContext context) {
-        return false;
-    }
+        // only 1 font should be used
+        Assert.assertTrue(fontFrequency.size() == 1);
 
-    /* (non-Javadoc)
-     * @see com.itextpdf.html2pdf.attach.ITagWorker#getElementResult()
-     */
-    @Override
-    public IPropertyContainer getElementResult() {
-        return newLine;
+        // the font should be freesans
+        String usedFont = fontFrequency.keySet().iterator().next();
+        Assert.assertTrue(usedFont.equals("freesans"));
+
+        // it should have been used for 3 elements
+        Assert.assertTrue(fontFrequency.get(usedFont) == 3);
     }
 }
