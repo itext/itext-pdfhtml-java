f16172e3e9a01953a2ed6e298e0282486fa0dc22
Yulian Gaponenko <yulian.gaponenko@duallab.com>
2017-07-19 12:14:28 +0300

    Avoid possible class loading deadlock when subclass is referenced from superclass initializer
    When classes refer to their own sublcasses in their static initializer
    or in static fields, such references can cause JVM-level deadlocks in
    multithreaded environment, when one thread tries to load superclass and
    another thread tries to load subclass at the same time.

--- src/main/java/com/itextpdf/html2pdf/css/selector/item/CssPseudoClassSelectorItem.java
+++ src/main/java/com/itextpdf/html2pdf/css/selector/item/CssPseudoClassSelectorItem.java
@@ -2,7 +2,7 @@
     This file is part of the iText (R) project.
     Copyright (c) 1998-2017 iText Group NV
     Authors: Bruno Lowagie, Paulo Soares, et al.
-    
+
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License version 3
     as published by the Free Software Foundation with the addition of the
@@ -10,7 +10,7 @@
     FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY
     ITEXT GROUP. ITEXT GROUP DISCLAIMS THE WARRANTY OF NON INFRINGEMENT
     OF THIRD PARTY RIGHTS
-    
+
     This program is distributed in the hope that it will be useful, but
     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
     or FITNESS FOR A PARTICULAR PURPOSE.
@@ -20,15 +20,15 @@
     the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
     Boston, MA, 02110-1301 USA, or download the license from the following URL:
     http://itextpdf.com/terms-of-use/
-    
+
     The interactive user interfaces in modified source and object code versions
     of this program must display Appropriate Legal Notices, as required under
     Section 5 of the GNU Affero General Public License.
-    
+
     In accordance with Section 7(b) of the GNU Affero General Public License,
     a covered work must retain the producer line in every PDF that is created
     or manipulated using iText.
-    
+
     You can be released from the requirements of the license by purchasing
     a commercial license. Buying such a license is mandatory as soon as you
     develop commercial activities involving the iText software without
@@ -36,7 +36,7 @@
     These activities include: offering paid services to customers as an ASP,
     serving PDFs on the fly in a web application, shipping iText with a closed
     source product.
-    
+
     For more information, please contact iText Software Corp. at this
     address: sales@itextpdf.com
  */
@@ -60,12 +60,9 @@ public class CssPseudoClassSelectorItem implements ICssSelectorItem {
 
     /** The pseudo class. */
     protected String pseudoClass;
-    
+
     /** The arguments. */
     protected String arguments;
-
-    private static final FirstChildSelectorItem firstChild = new FirstChildSelectorItem();
-    private static final LastChildSelectorItem lastChild = new LastChildSelectorItem();
 
     /**
      * Creates a new {@link CssPseudoClassSelectorItem} instance.
@@ -101,9 +98,9 @@ public class CssPseudoClassSelectorItem implements ICssSelectorItem {
     public static CssPseudoClassSelectorItem create(String pseudoClass, String arguments) {
         switch (pseudoClass) {
             case CssConstants.FIRST_CHILD:
-                return firstChild;
+                return FirstChildSelectorItem.getInstance();
             case CssConstants.LAST_CHILD:
-                return lastChild;
+                return LastChildSelectorItem.getInstance();
             case CssConstants.NTH_CHILD:
                 return new NthChildSelectorItem(arguments);
             case CssConstants.NOT:
@@ -188,8 +185,14 @@ public class CssPseudoClassSelectorItem implements ICssSelectorItem {
     }
 
     private static class FirstChildSelectorItem extends ChildSelectorItem {
-        public FirstChildSelectorItem() {
+        private static final FirstChildSelectorItem instance = new FirstChildSelectorItem();
+
+        private FirstChildSelectorItem() {
             super(CssConstants.FIRST_CHILD);
+        }
+
+        public static FirstChildSelectorItem getInstance() {
+            return instance;
         }
 
         @Override
@@ -198,13 +201,19 @@ public class CssPseudoClassSelectorItem implements ICssSelectorItem {
                 return false;
             }
             List<INode> children = getAllSiblings(node);
-            return children.isEmpty() ? false : node.equals(children.get(0));
+            return !children.isEmpty() && node.equals(children.get(0));
         }
     }
 
     private static class LastChildSelectorItem extends ChildSelectorItem {
-        public LastChildSelectorItem() {
+        private static final LastChildSelectorItem instance = new LastChildSelectorItem();
+
+        private LastChildSelectorItem() {
             super(CssConstants.LAST_CHILD);
+        }
+
+        public static LastChildSelectorItem getInstance() {
+            return instance;
         }
 
         @Override
@@ -213,7 +222,7 @@ public class CssPseudoClassSelectorItem implements ICssSelectorItem {
                 return false;
             }
             List<INode> children = getAllSiblings(node);
-            return children.isEmpty() ? false : node.equals(children.get(children.size() - 1));
+            return !children.isEmpty() && node.equals(children.get(children.size() - 1));
         }
     }
 
@@ -235,7 +244,7 @@ public class CssPseudoClassSelectorItem implements ICssSelectorItem {
                 return false;
             }
             List<INode> children = getAllSiblings(node);
-            return children.isEmpty() ? false : resolveNthChild(node, children);
+            return !children.isEmpty() && resolveNthChild(node, children);
         }
 
         /**
@@ -287,10 +296,10 @@ public class CssPseudoClassSelectorItem implements ICssSelectorItem {
                 return false;
             if (this.nthChildA > 0) {
                 int temp = children.indexOf(node) + 1 - this.nthChildB;
-                return temp >= 0 ? temp % this.nthChildA == 0 : false;
+                return temp >= 0 && temp % this.nthChildA == 0;
             } else if (this.nthChildA < 0) {
                 int temp = children.indexOf(node) + 1 - this.nthChildB;
-                return temp <= 0 ? temp % this.nthChildA == 0 : false;
+                return temp <= 0 && temp % this.nthChildA == 0;
             } else
                 return (children.indexOf(node) + 1) - this.nthChildB == 0;
         }
