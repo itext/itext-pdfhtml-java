be85b10fbf6168e8ae1e3fc1575e9d75e10ba299
Valera Goman <valera.goman@duallab.com>
2017-07-20 19:05:21 +0300

    Adjust transform property parsing for new property class Transform
    DEVSIX-1310

--- src/main/java/com/itextpdf/html2pdf/css/apply/util/TransformationApplierUtil.java
+++ src/main/java/com/itextpdf/html2pdf/css/apply/util/TransformationApplierUtil.java
@@ -48,10 +48,10 @@ import com.itextpdf.html2pdf.css.util.CssUtils;
 import com.itextpdf.layout.IPropertyContainer;
 
 import com.itextpdf.layout.property.Property;
+import com.itextpdf.layout.property.Transform;
+import com.itextpdf.layout.property.UnitValue;
 
 import java.util.Map;
-import java.util.List;
-import java.util.ArrayList;
 
 import static java.lang.Math.toRadians;
 import static java.lang.Math.cos;
@@ -80,20 +80,20 @@ public class TransformationApplierUtil {
         else
             return;
         String[] components = transformationFunction.split("\\)");
-        List<String[]> multipleFunction = new ArrayList<>(components.length);
+        Transform multipleFunction = new Transform(components.length);
         for (String component : components) {
-            multipleFunction.add(parseSingleFunction(component));
+            multipleFunction.addSingleTransform(parseSingleFunction(component));
         }
         element.setProperty(Property.TRANSFORM, multipleFunction);
     }
 
-    private static String[] parseSingleFunction(String transformationFunction) {
+    private static Transform.SingleTransform parseSingleFunction(String transformationFunction) {
         String function, args;
         if (!CssConstants.NONE.equals(transformationFunction)) {
             function = transformationFunction.substring(0, transformationFunction.indexOf('(')).trim();
             args = transformationFunction.substring(transformationFunction.indexOf('(') + 1);
         } else {
-            return floatArrayToStringArray(new float[]{1, 0, 0, 1, 0, 0});
+            return getSingleTransform(1, 0, 0, 1, 0, 0);
         }
         if (CssConstants.MATRIX.equals(function)) {
             String[] arg = args.split(",");
@@ -108,59 +108,36 @@ public class TransformationApplierUtil {
                     if (i == 1 || i == 2 || i == 5)
                         matrix[i] *= -1;
                 }
-                return floatArrayToStringArray(matrix);
+                return getSingleTransform(matrix);
             }
         }
         if (CssConstants.TRANSLATE.equals(function)) {
             String[] arg = args.split(",");
-            String xStr = null, yStr = null;
-            float x = 0, y = 0;
-            if (arg[0].indexOf('%') > 0)
-                xStr = arg[0];
-            else
-                x = CssUtils.parseAbsoluteLength(arg[0].trim());
+            boolean xPoint, yPoint = true;
+            float x, y = 0;
+            xPoint = arg[0].indexOf('%') < 0;
+            x = xPoint ? CssUtils.parseAbsoluteLength(arg[0].trim()) : Float.parseFloat(arg[0].trim().substring(0, arg[0].indexOf('%')));
             if (arg.length == 2) {
-                if (arg[1].indexOf('%') > 0)
-                    yStr = Float.toString(-1 * Float.parseFloat(arg[1].substring(0, arg[1].indexOf('%')))) + '%';
-                else
-                    y = -1 * CssUtils.parseAbsoluteLength(arg[1].trim());
+                yPoint = arg[1].indexOf('%') < 0;
+                y = -1 * (yPoint ? CssUtils.parseAbsoluteLength(arg[1].trim()) : Float.parseFloat(arg[1].trim().substring(0, arg[1].indexOf('%'))));
             }
-            String[] transform = floatArrayToStringArray(new float[]{1, 0, 0, 1, x, y});
-            if (xStr != null)
-                transform[4] = xStr;
-            if (yStr != null)
-                transform[5] = yStr;
-            return transform;
+            return getSingleTransformTranslate(1, 0, 0, 1, x, y, xPoint, yPoint);
         }
         if (CssConstants.TRANSLATE_X.equals(function)) {
-            String xStr = null;
-            float x = 0;
-            if (args.indexOf('%') > 0)
-                xStr = args;
-            else
-                x = CssUtils.parseAbsoluteLength(args.trim());
-            String[] transform = floatArrayToStringArray(new float[]{1, 0, 0, 1, x, 0});
-            if (xStr != null)
-                transform[4] = xStr;
-            return transform;
+            boolean xPoint = args.indexOf('%') < 0;
+            float x = xPoint ? CssUtils.parseAbsoluteLength(args.trim()) : Float.parseFloat(args.trim().substring(0, args.indexOf('%')));;
+            return getSingleTransformTranslate(1, 0, 0, 1, x, 0, xPoint, true);
         }
         if (CssConstants.TRANSLATE_Y.equals(function)) {
-            String yStr = null;
-            float y = 0;
-            if (args.indexOf('%') > 0)
-                yStr = Float.toString(-1 * Float.parseFloat(args.substring(0, args.indexOf('%')))) + '%';
-            else
-                y = -1 * CssUtils.parseAbsoluteLength(args.trim());
-            String[] transform = floatArrayToStringArray(new float[]{1, 0, 0, 1, 0, y});
-            if (yStr != null)
-                transform[4] = yStr;
-            return transform;
+            boolean yPoint = args.indexOf('%') < 0;
+            float y = -1 * (yPoint ? CssUtils.parseAbsoluteLength(args.trim()) : Float.parseFloat(args.trim().substring(0, args.indexOf('%'))));
+            return getSingleTransformTranslate(1, 0, 0, 1, 0, y, true, yPoint);
         }
         if (CssConstants.ROTATE.equals(function)) {
             double angleInRad = parseAngleToRadians(args);
             float cos = (float) cos(angleInRad);
             float sin = (float) sin(angleInRad);
-            return floatArrayToStringArray(new float[]{cos, sin, -1 * sin, cos, 0, 0});
+            return getSingleTransform(cos, sin, -1 * sin, cos, 0, 0);
         }
         if (CssConstants.SKEW.equals(function)) {
             String[] arg = args.split(",");
@@ -168,15 +145,15 @@ public class TransformationApplierUtil {
                     yAngleInRad = arg.length == 2 ? parseAngleToRadians(arg[1]) : 0.0;
             float tanX = (float) tan(xAngleInRad);
             float tanY = (float) tan(yAngleInRad);
-            return floatArrayToStringArray(new float[]{1, tanY, tanX, 1, 0, 0});
+            return getSingleTransform(1, tanY, tanX, 1, 0, 0);
         }
         if (CssConstants.SKEW_X.equals(function)) {
             float tanX = (float) tan(parseAngleToRadians(args));
-            return floatArrayToStringArray(new float[]{1, 0, tanX, 1, 0, 0});
+            return getSingleTransform(1, 0, tanX, 1, 0, 0);
         }
         if (CssConstants.SKEW_Y.equals(function)) {
             float tanY = (float) tan(parseAngleToRadians(args));
-            return floatArrayToStringArray(new float[]{1, tanY, 0, 1, 0, 0});
+            return getSingleTransform(1, tanY, 0, 1, 0, 0);
         }
         if (CssConstants.SCALE.equals(function)) {
             String[] arg = args.split(",");
@@ -188,17 +165,17 @@ public class TransformationApplierUtil {
                 x = Float.parseFloat(arg[0].trim());
                 y = x;
             }
-            return floatArrayToStringArray(new float[]{x, 0, 0, y, 0, 0});
+            return getSingleTransform(x, 0, 0, y, 0, 0);
         }
         if (CssConstants.SCALE_X.equals(function)) {
             float x = Float.parseFloat(args.trim());
-            return floatArrayToStringArray(new float[]{x, 0, 0, 1, 0, 0});
+            return getSingleTransform(x, 0, 0, 1, 0, 0);
         }
         if (CssConstants.SCALE_Y.equals(function)) {
             float y = Float.parseFloat(args.trim());
-            return floatArrayToStringArray(new float[]{1, 0, 0, y, 0, 0});
+            return getSingleTransform(1, 0, 0, y, 0, 0);
         }
-        return new String[6];
+        return new Transform.SingleTransform();
     }
 
     private static double parseAngleToRadians(String value){
@@ -209,10 +186,18 @@ public class TransformationApplierUtil {
         return toRadians(-1 * Double.parseDouble(value.trim().substring(0, value.indexOf('d'))));
     }
 
-    private static String[] floatArrayToStringArray(float[] floats) {
-        String[] strings = new String[floats.length];
-        for (int i = 0; i < floats.length; i++)
-            strings[i] = Float.toString(floats[i]);
-        return strings;
+    private static Transform.SingleTransform getSingleTransformTranslate(float a, float b, float c, float d, float tx, float ty, boolean xPoint, boolean yPoint) {
+        return new Transform.SingleTransform(a, b, c, d,
+                new UnitValue(xPoint ? UnitValue.POINT : UnitValue.PERCENT, tx), new UnitValue(yPoint ? UnitValue.POINT : UnitValue.PERCENT, ty));
+    }
+
+    private static Transform.SingleTransform getSingleTransform(float a, float b, float c, float d, float tx, float ty) {
+        return new Transform.SingleTransform(a, b, c, d,
+                new UnitValue(UnitValue.POINT, tx), new UnitValue(UnitValue.POINT, ty));
+    }
+
+    private static Transform.SingleTransform getSingleTransform(float floats[]) {
+        return new Transform.SingleTransform(floats[0], floats[1], floats[2], floats[3],
+                new UnitValue(UnitValue.POINT, floats[4]), new UnitValue(UnitValue.POINT, floats[5]));
     }
 }
