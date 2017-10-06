5767a780a86fa36e2f5308e3c2cd2f6123493ec6
Samuel Huylebroeck <samuel.huylebroeck@itextpdf.com>
2017-10-06 15:45:14 +0200

    Support start ol attribute
    DEVSIX-1611

--- src/test/java/com/itextpdf/html2pdf/css/ListCssTest.java
+++ src/test/java/com/itextpdf/html2pdf/css/ListCssTest.java
@@ -1,8 +1,8 @@
 /*
     This file is part of the iText (R) project.
     Copyright (c) 1998-2017 iText Group NV
-    Authors: Bruno Lowagie, Paulo Soares, et al.
-    
+    Authors: iText Software.
+
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License version 3
     as published by the Free Software Foundation with the addition of the
@@ -10,7 +10,7 @@
     FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY
     ITEXT GROUP. ITEXT GROUP DISCLAIMS THE WARRANTY OF NON INFRINGEMENT
     OF THIRD PARTY RIGHTS
-    
+
     This program is distributed in the hope that it will be useful, but
     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
     or FITNESS FOR A PARTICULAR PURPOSE.
@@ -20,15 +20,15 @@
     the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
     Boston, MA, 02110-1301 USA, or download the license from the following URL:
     http://itextpdf.com/terms-of-use/
-    
+
     The interactive user interfaces in modified source and object code versions
     of this program must display Appropriate Legal Notices, as required under
     Section 5 of the GNU Affero General Public License.
-    
+
     In accordance with Section 7(b) of the GNU Affero General Public License,
     a covered work must retain the producer line in every PDF that is created
     or manipulated using iText.
-    
+
     You can be released from the requirements of the license by purchasing
     a commercial license. Buying such a license is mandatory as soon as you
     develop commercial activities involving the iText software without
@@ -36,57 +36,38 @@
     These activities include: offering paid services to customers as an ASP,
     serving PDFs on the fly in a web application, shipping iText with a closed
     source product.
-    
+
     For more information, please contact iText Software Corp. at this
     address: sales@itextpdf.com
  */
-package com.itextpdf.html2pdf.css.apply.impl;
+package com.itextpdf.html2pdf.css;
 
-import com.itextpdf.html2pdf.attach.ITagWorker;
-import com.itextpdf.html2pdf.attach.ProcessorContext;
-import com.itextpdf.html2pdf.css.CssConstants;
-import com.itextpdf.html2pdf.css.apply.ICssApplier;
-import com.itextpdf.html2pdf.css.apply.util.ListStyleApplierUtil;
-import com.itextpdf.html2pdf.html.node.IStylesContainer;
-import com.itextpdf.layout.element.List;
-import com.itextpdf.layout.property.ListSymbolPosition;
-import com.itextpdf.layout.property.Property;
-import java.util.Map;
+import com.itextpdf.html2pdf.HtmlConverter;
+import com.itextpdf.kernel.utils.CompareTool;
+import com.itextpdf.test.ExtendedITextTest;
+import com.itextpdf.test.annotations.type.IntegrationTest;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
-/**
- * {@link ICssApplier} implementation for Ul en Ol elements.
- */
-public class UlOlTagCssApplier extends BlockCssApplier {
+import java.io.File;
+import java.io.IOException;
 
-    /* (non-Javadoc)
-     * @see com.itextpdf.html2pdf.css.apply.impl.BlockCssApplier#apply(com.itextpdf.html2pdf.attach.ProcessorContext, com.itextpdf.html2pdf.html.node.IStylesContainer, com.itextpdf.html2pdf.attach.ITagWorker)
-     */
-    @Override
-    public void apply(ProcessorContext context, IStylesContainer stylesContainer, ITagWorker tagWorker) {
-        if (!(tagWorker.getElementResult() instanceof List)) {
-            return;
-        }
-        Map<String, String> css = stylesContainer.getStyles();
+@Category(IntegrationTest.class)
+public class ListCssTest extends ExtendedITextTest {
+    public static final String sourceFolder = "./src/test/resources/com/itextpdf/html2pdf/css/ListCSSTest/";
+    public static final String destinationFolder = "./target/test/com/itextpdf/html2pdf/css/ListCSSTest/";
 
-        List list = (List) tagWorker.getElementResult();
-
-        if (CssConstants.INSIDE.equals(css.get(CssConstants.LIST_STYLE_POSITION))) {
-            list.setProperty(Property.LIST_SYMBOL_POSITION, ListSymbolPosition.INSIDE);
-        } else {
-            list.setProperty(Property.LIST_SYMBOL_POSITION, ListSymbolPosition.OUTSIDE);
-        }
-
-        ListStyleApplierUtil.applyListStyleTypeProperty(stylesContainer, css, context, list);
-        ListStyleApplierUtil.applyListStyleImageProperty(css, context, list);
-
-        //For an ordered list, check and potentially retrieve the start property
-        if(css.get(CssConstants.START) != null){
-            int startValue = Integer.parseInt(css.get(CssConstants.START));
-            list.setItemStartIndex(startValue);
-        }
-
-        super.apply(context, stylesContainer, tagWorker);
+    @BeforeClass
+    public static void beforeClass() {
+        createDestinationFolder(destinationFolder);
     }
 
+    @Test
+    public void listCSSStartTest01() throws IOException, InterruptedException {
+        HtmlConverter.convertToPdf(new File(sourceFolder + "orderedList.html"), new File(destinationFolder + "orderedList01.pdf"));
+        Assert.assertNull(new CompareTool().compareByContent(destinationFolder + "orderedList01.pdf", sourceFolder + "cmp_orderedList01.pdf", destinationFolder, "diff02_"));
 
+    }
 }
