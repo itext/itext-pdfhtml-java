7a4cc798a61ffc3a6bf1d998bf431b5b441f5336
Alexey Subach <alexey.subach@duallab.com>
2017-10-10 15:46:34 +0300

    Add a link to Jira ticket in TODO

--- src/main/java/com/itextpdf/html2pdf/attach/impl/layout/form/renderer/AbstractTextFieldRenderer.java
+++ src/main/java/com/itextpdf/html2pdf/attach/impl/layout/form/renderer/AbstractTextFieldRenderer.java
@@ -86,7 +86,7 @@ public abstract class AbstractTextFieldRenderer extends AbstractFormFieldRendere
      */
     IRenderer createParagraphRenderer(String defaultValue) {
         if (defaultValue.trim().isEmpty()) {
-            // TODO: change to 'defaultValue = "\u00A0"' after trimming of non-breakable spaces is fixed;
+            // TODO DEVSIX-1491: change to 'defaultValue = "\u00A0"' after trimming of non-breakable spaces is fixed;
             defaultValue = "\u00B7";
         }
         Paragraph paragraph = new Paragraph(defaultValue).setMargin(0);
