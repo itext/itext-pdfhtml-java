d811d2d1b84c3c9c1bb0405040307d0892b8d8c6
Yulian Gaponenko <yulian.gaponenko@duallab.com>
2018-04-09 12:58:17 +0300

    Unignore test
    DEVSIX-1123

--- src/test/java/com/itextpdf/html2pdf/css/w3c/css21/generated_content/ContentCounters003Test.java
+++ src/test/java/com/itextpdf/html2pdf/css/w3c/css21/generated_content/ContentCounters003Test.java
@@ -45,7 +45,17 @@ package com.itextpdf.html2pdf.css.w3c.css21.generated_content;
 import com.itextpdf.html2pdf.css.w3c.W3CCssTest;
 import org.junit.Ignore;
 
-@Ignore("DEVSIX-1123")
+/**
+ * Difference in width of two lines is due the our approach of fonts resolving.
+ * In first line, every span (and spaces in-between them) is a separate text chunk, for which
+ * fonts are resolved separately. Thus spaces end up in TimesRoman font, while discs font is
+ * resolved to FreeMono. The second line is a single text chunk, which is wholly resolved to FreeMono.
+ * This creates difference in lines width.
+ *
+ * The issue in the first place is that by default we should not resolve fonts to mono generic family,
+ * which will be supported under DEVSIX-1034.
+ *
+ */
 public class ContentCounters003Test extends W3CCssTest {
     @Override
     protected String getHtmlFileName() {
