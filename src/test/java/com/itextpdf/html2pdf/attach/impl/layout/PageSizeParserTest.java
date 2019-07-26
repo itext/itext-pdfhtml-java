bf3279b13989826e6667c151a96cd39de6424bb9
Alexey Subach <alexey.subach@duallab.com>
2019-07-26 13:59:49 +0300

    Add new unit test for PageSizeParser
    A couple of tests demonstrate a problem with parsing size declaration from @page media
    
    DEVSIX-3072

---
+++ src/test/java/com/itextpdf/html2pdf/attach/impl/layout/PageSizeParserTest.java
@@ -0,0 +1,50 @@
+package com.itextpdf.html2pdf.attach.impl.layout;
+
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.test.annotations.type.UnitTest;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(UnitTest.class)
+public class PageSizeParserTest {
+
+    private static final double EPS = 1e-9;
+
+    @Test
+    public void simpleA4Test() {
+        PageSize expected = PageSize.A4;
+        PageSize actual = PageSizeParser.fetchPageSize("a4", 10, 10, PageSize.A0);
+        assertSizesAreSame(expected, actual);
+    }
+
+    @Test
+    public void simpleLedgerTest() {
+        PageSize expected = PageSize.LEDGER;
+        PageSize actual = PageSizeParser.fetchPageSize("ledger", 10, 10, PageSize.A0);
+        assertSizesAreSame(expected, actual);
+    }
+
+    @Test
+    @Ignore("To be fixed in DEVSIX-3072")
+    public void ledgerLandscapeIsSameAsLedgerTest() {
+        PageSize expected = PageSize.LEDGER;
+        PageSize actual = PageSizeParser.fetchPageSize("ledger landscape", 10, 10, PageSize.A0);
+        assertSizesAreSame(expected, actual);
+    }
+
+    @Test
+    @Ignore("To be fixed in DEVSIX-3072")
+    public void ledgerPortraitIsRotatedLedgerTest() {
+        PageSize expected = PageSize.LEDGER.rotate();
+        PageSize actual = PageSizeParser.fetchPageSize("ledger portrait", 10, 10, PageSize.A0);
+        assertSizesAreSame(expected, actual);
+    }
+
+    private void assertSizesAreSame(PageSize a, PageSize b) {
+        Assert.assertEquals(a.getWidth(), b.getWidth(), EPS);
+        Assert.assertEquals(a.getHeight(), b.getHeight(), EPS);
+    }
+
+}
