9e4818b598a0f9f62938f041586a0381b42cc4f5
Kate Ivanova <ekaterina.ivanova@duallab.com>
2020-07-29 16:19:12 +0300

    Add remaining w3c linebox tests
    DEVSIX-4185

---
+++ src/test/java/com/itextpdf/html2pdf/css/w3c/css21/linebox/VerticalAlignSuper001Test.java
@@ -0,0 +1,47 @@
+/*
+    This file is part of the iText (R) project.
+    Copyright (c) 1998-2020 iText Group NV
+    Authors: iText Software.
+
+    This program is offered under a commercial and under the AGPL license.
+    For commercial licensing, contact us at https://itextpdf.com/sales.  For AGPL licensing, see below.
+
+    AGPL licensing:
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.itextpdf.html2pdf.css.w3c.css21.linebox;
+
+import com.itextpdf.html2pdf.css.w3c.W3CCssAhemFontTest;
+import com.itextpdf.test.annotations.LogMessage;
+import com.itextpdf.test.annotations.LogMessages;
+
+import java.io.IOException;
+import org.junit.Test;
+
+public class VerticalAlignSuper001Test extends W3CCssAhemFontTest {
+    @Override
+    protected String getHtmlFileName() {
+        return "vertical-align-super-001.xht";
+    }
+
+    @Test
+    // TODO DEVSIX-3757 update cmp file after completing the ticket
+    @Override
+    @LogMessages(messages = {
+            @LogMessage(messageTemplate = com.itextpdf.io.LogMessageConstant.OCCUPIED_AREA_HAS_NOT_BEEN_INITIALIZED, count = 2),
+    })
+    public void test() throws IOException, InterruptedException {
+        super.test();
+    }
+}
