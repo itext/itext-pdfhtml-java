323ba77a199bd9a62c04d5189b643079033c5051
Uladzimir Asipchuk <vladimir.osipchuk@duallab.com>
2020-08-12 13:35:06 +0300

    Add box-shadow tests
    DEVSIX-4376

---
+++ src/test/java/com/itextpdf/html2pdf/css/w3c/css_backgrounds/box_shadow/BoxShadowBlurDefinition001Test.java
@@ -0,0 +1,11 @@
+package com.itextpdf.html2pdf.css.w3c.css_backgrounds.box_shadow;
+
+import com.itextpdf.html2pdf.css.w3c.W3CCssTest;
+
+// TODO DEVSIX-4384 box-shadow is not supported
+public class BoxShadowBlurDefinition001Test extends W3CCssTest {
+    @Override
+    protected String getHtmlFileName() {
+        return "box-shadow-blur-definition-001.xht";
+    }
+}
