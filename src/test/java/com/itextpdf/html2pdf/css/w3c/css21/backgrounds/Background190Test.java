872b688c3b9a10abe33f94492bfe9478c9e0204e
Alex Rasolko <alex.rasolko@duallab.com>
2020-08-13 12:26:56 +0300

    Add background properties tests group css2 - PART_2
    DEVSIX-4346

---
+++ src/test/java/com/itextpdf/html2pdf/css/w3c/css21/backgrounds/Background190Test.java
@@ -0,0 +1,12 @@
+package com.itextpdf.html2pdf.css.w3c.css21.backgrounds;
+
+import com.itextpdf.html2pdf.css.w3c.W3CCssTest;
+
+// TODO DEVSIX-1457. Background-position is not supported: currently we apply background from the top
+//  and not from the bottom of the area
+public class Background190Test extends W3CCssTest {
+    @Override
+    protected String getHtmlFileName() {
+        return "background-190.xht";
+    }
+}
