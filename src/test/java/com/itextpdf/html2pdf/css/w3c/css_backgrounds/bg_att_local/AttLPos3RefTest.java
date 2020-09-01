ed90bf198eba699cce7d6fb50dcba4da13947ae1
Uladzimir Asipchuk <vladimir.osipchuk@duallab.com>
2020-09-01 10:38:29 +0300

    Update the test's TODO remark
    DEVSIX-4441

--- src/test/java/com/itextpdf/html2pdf/css/w3c/css_backgrounds/bg_att_local/AttLPos3RefTest.java
+++ src/test/java/com/itextpdf/html2pdf/css/w3c/css_backgrounds/bg_att_local/AttLPos3RefTest.java
@@ -24,7 +24,7 @@ package com.itextpdf.html2pdf.css.w3c.css_backgrounds.bg_att_local;
 
 import com.itextpdf.html2pdf.css.w3c.W3CCssTest;
 
-// TODO DEVSIX-4401 background: no-repeat is aligned to the bottom-right corner instead of the top-left one
+// TODO DEVSIX-1457 background-position is not supported
 public class AttLPos3RefTest extends W3CCssTest {
     @Override
     protected String getHtmlFileName() {
