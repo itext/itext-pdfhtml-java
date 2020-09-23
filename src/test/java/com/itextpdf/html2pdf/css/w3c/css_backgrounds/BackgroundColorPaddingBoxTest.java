371b77e2bc2047d7135b0a4ca09ef7ab0cf349c8
Alexey Subach <alexey.subach@itextpdf.com>
2020-09-23 16:56:49 +0300

    Remove obsolete TODO
    DEVSIX-4389

--- src/test/java/com/itextpdf/html2pdf/css/w3c/css_backgrounds/BackgroundColorPaddingBoxTest.java
+++ src/test/java/com/itextpdf/html2pdf/css/w3c/css_backgrounds/BackgroundColorPaddingBoxTest.java
@@ -25,7 +25,6 @@ package com.itextpdf.html2pdf.css.w3c.css_backgrounds;
 import com.itextpdf.html2pdf.css.w3c.W3CCssTest;
 
 // TODO DEVSIX-2105 support background-clip
-// TODO DEVSIX-4389 support background-color
 public class BackgroundColorPaddingBoxTest extends W3CCssTest {
     @Override
     protected String getHtmlFileName() {
