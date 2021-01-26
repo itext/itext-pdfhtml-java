13ba9f6484c15c46e5a95a08e895fdaf0038479b
Nikita Kovaliov <nikita.kovaliov@duallab.com>
2021-01-26 16:47:30 +0300

    Add flexbox-justify-content tests
    DEVSIX-5014

---
+++ src/test/java/com/itextpdf/html2pdf/css/w3c/css_flexbox/JustifyContentHoriz002Test.java
@@ -0,0 +1,11 @@
+package com.itextpdf.html2pdf.css.w3c.css_flexbox;
+
+import com.itextpdf.html2pdf.css.w3c.W3CCssTest;
+
+//TODO DEVSIX-1315 Initial support for flex display:flex CSS property
+public class JustifyContentHoriz002Test extends W3CCssTest {
+    @Override
+    protected String getHtmlFileName() {
+        return "flexbox-justify-content-horiz-002.xhtml";
+    }
+}
