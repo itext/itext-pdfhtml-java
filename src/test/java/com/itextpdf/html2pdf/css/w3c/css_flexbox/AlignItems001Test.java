f32d85e747989147ab347f11c296668b58352529
Nikita Kovaliov <nikita.kovaliov@duallab.com>
2021-02-12 01:42:06 +0300

    Add flexbox align-items and overflow tests
    DEVSIX-5060

---
+++ src/test/java/com/itextpdf/html2pdf/css/w3c/css_flexbox/AlignItems001Test.java
@@ -0,0 +1,19 @@
+package com.itextpdf.html2pdf.css.w3c.css_flexbox;
+
+import com.itextpdf.html2pdf.LogMessageConstant;
+import com.itextpdf.html2pdf.css.w3c.W3CCssTest;
+import com.itextpdf.test.LogLevelConstants;
+import com.itextpdf.test.annotations.LogMessage;
+import com.itextpdf.test.annotations.LogMessages;
+
+@LogMessages(messages = {
+        @LogMessage(messageTemplate = LogMessageConstant.INVALID_GRADIENT_DECLARATION, logLevel =
+                LogLevelConstants.WARN)
+})
+//TODO DEVSIX-1315 Initial support for flex display:flex CSS property
+public class AlignItems001Test extends W3CCssTest {
+    @Override
+    protected String getHtmlFileName() {
+        return "align-items-001.htm";
+    }
+}
