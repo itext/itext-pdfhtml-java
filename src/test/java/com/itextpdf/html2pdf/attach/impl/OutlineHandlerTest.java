13b6a963aba265eb27ac6cf39a6b8b51bdf71710
Alexey Subach <alexey.subach@itextpdf.com>
2021-03-11 17:50:36 +0300

    Add test demonstrating the problem of collision of internal doument links with OutlineHandler
    It reproduces when there is a link to an <h1-h6> element
    
    DEVSIX-5195

--- src/test/java/com/itextpdf/html2pdf/attach/impl/OutlineHandlerTest.java
+++ src/test/java/com/itextpdf/html2pdf/attach/impl/OutlineHandlerTest.java
@@ -24,6 +24,7 @@ package com.itextpdf.html2pdf.attach.impl;
 
 import com.itextpdf.html2pdf.ConverterProperties;
 import com.itextpdf.html2pdf.ExtendedHtmlConversionITextTest;
+import com.itextpdf.html2pdf.HtmlConverter;
 import com.itextpdf.html2pdf.attach.ProcessorContext;
 import com.itextpdf.html2pdf.attach.impl.tags.PTagWorker;
 import com.itextpdf.html2pdf.css.CssConstants;
@@ -32,6 +33,7 @@ import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfOutline;
 import com.itextpdf.kernel.pdf.PdfString;
 import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.utils.CompareTool;
 import com.itextpdf.styledxmlparser.jsoup.nodes.Element;
 import com.itextpdf.styledxmlparser.jsoup.parser.Tag;
 import com.itextpdf.styledxmlparser.node.IElementNode;
@@ -39,14 +41,25 @@ import com.itextpdf.styledxmlparser.node.impl.jsoup.node.JsoupElementNode;
 import com.itextpdf.test.annotations.type.IntegrationTest;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 @Category(IntegrationTest.class)
 public class OutlineHandlerTest extends ExtendedHtmlConversionITextTest {
+
+    private static final String SOURCE_FOLDER = "./src/test/resources/com/itextpdf/html2pdf/attach/impl/OutlineHandlerTest/";
+    private static final String DESTINATION_FOLDER = "./target/test/com/itextpdf/html2pdf/attach/impl/OutlineHandlerTest/";
+
+    @BeforeClass
+    public static void beforeClass() {
+        createOrClearDestinationFolder(DESTINATION_FOLDER);
+    }
 
     @Test
     public void defaultDestinationPrefixTest() {
@@ -98,4 +111,18 @@ public class OutlineHandlerTest extends ExtendedHtmlConversionITextTest {
         PdfString pdfStringDest = (PdfString) pdfOutline.getDestination().getPdfObject();
         Assert.assertEquals("prefix-1", pdfStringDest.toUnicodeString());
     }
+
+    @Test
+    // TODO DEVSIX-5195 fix cmp after fix is introduced
+    public void defaultOutlineHandlerWithHTagHavingIdTest() throws IOException, InterruptedException {
+        String inFile = SOURCE_FOLDER + "defaultOutlineHandlerWithHTagHavingIdTest.html";
+        String outFile = DESTINATION_FOLDER + "defaultOutlineHandlerWithHTagHavingIdTest.pdf";
+        String cmpFile = SOURCE_FOLDER + "cmp_defaultOutlineHandlerWithHTagHavingIdTest.pdf";
+
+        OutlineHandler outlineHandler = OutlineHandler.createStandardHandler();
+        HtmlConverter.convertToPdf(new File(inFile), new File(outFile),
+                new ConverterProperties().setOutlineHandler(outlineHandler));
+        Assert.assertNull(new CompareTool().compareByContent(outFile, cmpFile, DESTINATION_FOLDER, "diff_defaultOutlineHandlerWithHTagHavingIdTest"));
+    }
+
 }
