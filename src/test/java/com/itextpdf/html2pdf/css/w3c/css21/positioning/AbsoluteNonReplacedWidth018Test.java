b964ecaca4c9c206592a8023344361f6af124a47
Nikita Kovaliov <nikita.kovaliov@duallab.com>
2021-03-13 15:48:54 +0300

    Reorganize AbsoluteNonReplacedWidth and PositionRelative tests
    DEVSIX-5175

---
+++ src/test/java/com/itextpdf/html2pdf/css/w3c/css21/positioning/AbsoluteNonReplacedWidth018Test.java
@@ -0,0 +1,14 @@
+package com.itextpdf.html2pdf.css.w3c.css21.positioning;
+
+import com.itextpdf.html2pdf.css.w3c.W3CCssTest;
+import com.itextpdf.io.LogMessageConstant;
+import com.itextpdf.test.annotations.LogMessage;
+import com.itextpdf.test.annotations.LogMessages;
+
+@LogMessages(messages = @LogMessage(messageTemplate = LogMessageConstant.TYPOGRAPHY_NOT_FOUND, count = 14))
+public class AbsoluteNonReplacedWidth018Test extends W3CCssTest {
+    @Override
+    protected String getHtmlFileName() {
+        return "absolute-non-replaced-width-018.xht";
+    }
+}
