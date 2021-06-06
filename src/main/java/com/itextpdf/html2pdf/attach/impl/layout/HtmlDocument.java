65c57aee596ba14d0ac28e5da6da2d8fb1c7b1a4
Alexey Subach <alexey.subach@itextpdf.com>
2021-06-06 17:17:56 +0300

    Move target counter handler preparation on relayot from HtmlDocument to Document
    DEVSIX-5502

--- src/main/java/com/itextpdf/html2pdf/attach/impl/layout/HtmlDocument.java
+++ src/main/java/com/itextpdf/html2pdf/attach/impl/layout/HtmlDocument.java
@@ -52,7 +52,6 @@ public class HtmlDocument extends Document {
     public void relayout() {
         if (rootRenderer instanceof HtmlDocumentRenderer) {
             ((HtmlDocumentRenderer) rootRenderer).removeEventHandlers();
-            ((HtmlDocumentRenderer) rootRenderer).getTargetCounterHandler().prepareHandlerToRelayout();
             super.relayout();
             ((HtmlDocumentRenderer) rootRenderer).processWaitingElement();
         }
