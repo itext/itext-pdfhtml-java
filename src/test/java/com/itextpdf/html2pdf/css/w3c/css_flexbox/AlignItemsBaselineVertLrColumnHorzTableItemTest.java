646d397eb073cf40499ba97ee83341ea3b1e8598
Nanou Persoons <nanou.persoons@apryse.com>
2023-06-20 10:49:19 +0200

    Go through all W3C test files and add tests
    DEVSIX-7590

---
+++ src/test/java/com/itextpdf/html2pdf/css/w3c/css_flexbox/AlignItemsBaselineVertLrColumnHorzTableItemTest.java
@@ -0,0 +1,43 @@
+/*
+    This file is part of the iText (R) project.
+    Copyright (c) 1998-2023 Apryse Group NV
+    Authors: Apryse Software.
+
+    This program is offered under a commercial and under the AGPL license.
+    For commercial licensing, contact us at https://itextpdf.com/sales.  For AGPL licensing, see below.
+
+    AGPL licensing:
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.itextpdf.html2pdf.css.w3c.css_flexbox;
+
+import com.itextpdf.html2pdf.css.w3c.W3CCssTest;
+import com.itextpdf.html2pdf.logs.Html2PdfLogMessageConstant;
+import com.itextpdf.io.logs.IoLogMessageConstant;
+import com.itextpdf.test.annotations.LogMessage;
+import com.itextpdf.test.annotations.LogMessages;
+
+//TODO DEVSIX-5167 change after align-items:baseline is supported
+//TODO DEVSIX-5096 change after flex-direction: column is supported
+@LogMessages(messages = {
+        @LogMessage(messageTemplate = Html2PdfLogMessageConstant.FLEX_PROPERTY_IS_NOT_SUPPORTED_YET, count = 2),
+        @LogMessage(messageTemplate = Html2PdfLogMessageConstant.NO_WORKER_FOUND_FOR_TAG, count = 3),
+        @LogMessage(messageTemplate = IoLogMessageConstant.TABLE_WIDTH_IS_MORE_THAN_EXPECTED_DUE_TO_MIN_WIDTH, count = 2)
+})
+public class AlignItemsBaselineVertLrColumnHorzTableItemTest extends W3CCssTest {
+    @Override
+    protected String getHtmlFileName() {
+        return "align-items-baseline-vert-lr-column-horz-table-item.html";
+    }
+}
