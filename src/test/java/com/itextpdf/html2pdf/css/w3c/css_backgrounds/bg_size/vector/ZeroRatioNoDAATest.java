41278026d82523228b98a22b8168ee60cc5c1fbd
Angelina Pavlovets <angelina.pavlovets@apryse.com>
2024-12-06 14:40:47 +0300

    Svg: improve viewBox applying for AbstractBranchSvgNodeRenderer
    DEVSIX-4861

--- src/test/java/com/itextpdf/html2pdf/css/w3c/css_backgrounds/bg_size/vector/ZeroRatioNoDAATest.java
+++ src/test/java/com/itextpdf/html2pdf/css/w3c/css_backgrounds/bg_size/vector/ZeroRatioNoDAATest.java
@@ -23,15 +23,8 @@
 package com.itextpdf.html2pdf.css.w3c.css_backgrounds.bg_size.vector;
 
 import com.itextpdf.html2pdf.css.w3c.W3CCssTest;
-import com.itextpdf.io.logs.IoLogMessageConstant;
-import com.itextpdf.styledxmlparser.logs.StyledXmlParserLogMessageConstant;
-import com.itextpdf.test.annotations.LogMessage;
-import com.itextpdf.test.annotations.LogMessages;
 
 // TODO DEVSIX-4388 percentage is not supported for rect's x, y, width and height
-@LogMessages(messages = {
-        @LogMessage(messageTemplate = IoLogMessageConstant.ATTEMPT_PROCESS_NAN, count = 9)
-})
 public class ZeroRatioNoDAATest extends W3CCssTest {
     @Override
     protected String getHtmlFileName() {
